a1a8b7f900d68049437bfc599231780c5a6b5b97
Maggie Summers <summers.maggie@gmail.com>
2018-02-21 14:21:14 -0800

    add doctor_form functionality

--- app.rb
+++ app.rb
@@ -6,3 +6,24 @@ require("./lib/patient")
 require("pg")
 
 DB = PG.connect({:dbname => "doc_office_test"})
+
+get('/') do
+  erb(:home)
+end
+
+get('/doctors') do
+  @doctors = Doctor.all()
+  erb(:doctors)
+end
+
+get('/doctors/new') do
+  erb(:doctor_form)
+end
+
+post('/doctors') do
+  name = params.fetch('name')
+  speciality = params.fetch('speciality')
+  doctor = Doctor.new({:name => name, :speciality => speciality, :id => nil})
+  doctor.save()
+  erb(:success)
+end
